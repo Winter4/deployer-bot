8f5a29e73241efe3dbf3272644220c6731e609ac
winter4 <mywinter4@gmail.com>
2025-01-18 02:05:24 +0300

    add bot handler

--- bot.ts
+++ bot.ts
@@ -1,5 +1,5 @@
 import 'dotenv/config';
-import { Bot, InlineKeyboard } from "grammy";
+import { Bot, InlineKeyboard, GrammyError, HttpError } from "grammy";
 import {exec} from 'child_process';
 
 if(!process.env.BOT_TOKEN) throw new Error('Empty .env bot_token');
@@ -139,6 +139,19 @@ bot.callbackQuery('v5', async ctx => {
     });
 
     return ctx.answerCallbackQuery('Делаю...')
+});
+
+bot.catch((err) => {
+    const ctx = err.ctx;
+    console.error(`Error while handling update ${ctx.update.update_id}:`);
+    const e = err.error;
+    if (e instanceof GrammyError) {
+        console.error("Error in request:", e.description);
+    } else if (e instanceof HttpError) {
+        console.error("Could not contact Telegram:", e);
+    } else {
+        console.error("Unknown error:", e);
+    }
 });
 
 bot.start();
